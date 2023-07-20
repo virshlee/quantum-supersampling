5a48f8ab5da0ac47713a3a4352d189ee8b86a0ee
virshlee <51300115+virshlee@users.noreply.github.com>
2023-07-20 11:48:13 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,212 @@
+from functools import lru_cache
+
+import pennylane as qml
+from pennylane import numpy as np
+import matplotlib.pyplot as plt
+from mygates import *
+
+shots = int(np.power(10, 7))
+dev1 = qml.device("default.qubit", wires=8, shots=shots)
+
+
+@qml.qnode(dev1, interface="autograd")
+def grover4():
+    qml.Hadamard(wires=0)
+    qml.Hadamard(wires=1)
+    qml.Hadamard(wires=2)
+    qml.Hadamard(wires=3)
+    Oracle(wires=[0, 1, 2, 3])
+    Diffuser(wires=[0, 1, 2, 3])
+    Oracle(wires=[0, 1, 2, 3])
+    Diffuser(wires=[0, 1, 2, 3])
+    Oracle(wires=[0, 1, 2, 3])
+    Diffuser(wires=[0, 1, 2, 3])
+    return qml.probs(wires=range(4))
+
+
+@qml.qnode(dev1, interface="autograd")
+def test4(params):
+    qml.RX(phi=np.pi * params[0], wires=0)
+    qml.RX(phi=np.pi * params[1], wires=1)
+    qml.RX(phi=np.pi * params[2], wires=2)
+    qml.RX(phi=np.pi * params[3], wires=3)
+    CCCZ(wires=range(4))
+    return qml.probs(wires=range(4))
+
+
+@qml.qnode(dev1, interface="autograd")
+def test():
+    qml.QFT(wires=[4, 5, 6])
+    qml.adjoint(qml.QFT(wires=[4, 5, 6]))
+    return qml.probs(wires=range(4))
+
+
+@qml.qnode(dev1, interface="autograd")
+def grover7(inversion):
+    qml.Hadamard(wires=0)
+    qml.Hadamard(wires=1)
+    qml.Hadamard(wires=2)
+    qml.Hadamard(wires=3)
+
+    qml.Hadamard(wires=4)
+    qml.Hadamard(wires=5)
+    qml.Hadamard(wires=6)
+
+    # qml.PauliX(wires=4)
+    # qml.PauliX(wires=5)
+    # qml.PauliX(wires=6)
+
+    Coracle(inversion=inversion, wires=[0, 1, 2, 3, 4])
+    Cdiffuser(wires=[0, 1, 2, 3, 4])
+
+    Coracle(inversion=inversion, wires=[0, 1, 2, 3, 5])
+    Cdiffuser(wires=[0, 1, 2, 3, 5])
+    Coracle(inversion=inversion, wires=[0, 1, 2, 3, 5])
+    Cdiffuser(wires=[0, 1, 2, 3, 5])
+
+    Coracle(inversion=inversion, wires=[0, 1, 2, 3, 6])
+    Cdiffuser(wires=[0, 1, 2, 3, 6])
+    Coracle(inversion=inversion, wires=[0, 1, 2, 3, 6])
+    Cdiffuser(wires=[0, 1, 2, 3, 6])
+    Coracle(inversion=inversion, wires=[0, 1, 2, 3, 6])
+    Cdiffuser(wires=[0, 1, 2, 3, 6])
+    Coracle(inversion=inversion, wires=[0, 1, 2, 3, 6])
+    Cdiffuser(wires=[0, 1, 2, 3, 6])
+
+    qml.adjoint(qml.QFT(wires=[4, 5, 6]))
+    # qml.Permute([0, 1, 2, 3, 6, 5, 4, 7], dev1.wires)
+    # qml.Permute([1, 2, 3, 0, 5, 4, 6, 7], dev1.wires)
+    qml.Permute([1, 2, 3, 0, 6, 5, 4, 7], dev1.wires)  # video
+    # return qml.probs(wires=[4, 5, 6])
+    return qml.probs(range(7))
+
+
+@qml.qnode(dev1, interface="autograd")
+def grover8(inversion):
+    # qml.PauliX(wires=0)
+    # qml.PauliX(wires=1)
+    # qml.PauliX(wires=2)
+    # qml.PauliX(wires=3)
+    # qml.PauliX(wires=4)
+    # qml.PauliX(wires=5)
+    # qml.PauliX(wires=6)
+    # qml.PauliX(wires=7)
+
+    qml.Hadamard(wires=0)
+    qml.Hadamard(wires=1)
+    qml.Hadamard(wires=2)
+    qml.Hadamard(wires=3)
+    qml.Hadamard(wires=4)
+    qml.Hadamard(wires=5)
+    qml.Hadamard(wires=6)
+    qml.Hadamard(wires=7)
+
+    Coracle(inversion=inversion, wires=[0, 1, 2, 3, 4])
+    Cdiffuser(wires=[0, 1, 2, 3, 4])
+
+    Coracle(inversion=inversion, wires=[0, 1, 2, 3, 5])
+    Cdiffuser(wires=[0, 1, 2, 3, 5])
+    Coracle(inversion=inversion, wires=[0, 1, 2, 3, 5])
+    Cdiffuser(wires=[0, 1, 2, 3, 5])
+
+    Coracle(inversion=inversion, wires=[0, 1, 2, 3, 6])
+    Cdiffuser(wires=[0, 1, 2, 3, 6])
+    Coracle(inversion=inversion, wires=[0, 1, 2, 3, 6])
+    Cdiffuser(wires=[0, 1, 2, 3, 6])
+    Coracle(inversion=inversion, wires=[0, 1, 2, 3, 6])
+    Cdiffuser(wires=[0, 1, 2, 3, 6])
+    Coracle(inversion=inversion, wires=[0, 1, 2, 3, 6])
+    Cdiffuser(wires=[0, 1, 2, 3, 6])
+
+    Coracle(inversion=inversion, wires=[0, 1, 2, 3, 7])
+    Cdiffuser(wires=[0, 1, 2, 3, 7])
+    Coracle(inversion=inversion, wires=[0, 1, 2, 3, 7])
+    Cdiffuser(wires=[0, 1, 2, 3, 7])
+    Coracle(inversion=inversion, wires=[0, 1, 2, 3, 7])
+    Cdiffuser(wires=[0, 1, 2, 3, 7])
+    Coracle(inversion=inversion, wires=[0, 1, 2, 3, 7])
+    Cdiffuser(wires=[0, 1, 2, 3, 7])
+    Coracle(inversion=inversion, wires=[0, 1, 2, 3, 7])
+    Cdiffuser(wires=[0, 1, 2, 3, 7])
+    Coracle(inversion=inversion, wires=[0, 1, 2, 3, 7])
+    Cdiffuser(wires=[0, 1, 2, 3, 7])
+    Coracle(inversion=inversion, wires=[0, 1, 2, 3, 7])
+    Cdiffuser(wires=[0, 1, 2, 3, 7])
+    Coracle(inversion=inversion, wires=[0, 1, 2, 3, 7])
+    Cdiffuser(wires=[0, 1, 2, 3, 7])
+
+    #qml.adjoint(qml.QFT(wires=[4, 5, 6, 7]))
+
+    qml.Permute([1, 2, 3, 0, 4, 5, 6, 7], dev1.wires)
+    return qml.probs(wires=range(8))
+
+
+def run8(inversion):
+    probs = grover8(inversion = inversion)
+    colormap = np.zeros((16, 16), dtype=float)
+    i = 0
+    j = 0
+    index = 0
+    for x in probs:
+        colormap[i, j] = x
+        print(index, x)
+        index = index + 1
+        i = (i + 1) % 16
+        if i % 16 == 0:
+            j = j + 1
+
+    fig, ax = plt.subplots()
+
+    # ax.yaxis.set_major_formatter(StrMethodFormatter("{x:03b}"))
+    ax.yaxis.set_ticks(range(16))
+    # ax.xaxis.set_major_formatter(StrMethodFormatter("{x:04b}"))
+    ax.xaxis.set_ticks(range(16))
+
+    # plt.ylabel("")
+    plt.xlabel("pixel index")
+    plt.ylabel("grover iteration number")
+
+    plt.imshow(colormap, cmap="Blues", interpolation='nearest')
+    cax = plt.axes([0.85, 0.1, 0.075, 0.8])
+    plt.colorbar(cax=cax)
+    plt.show()
+    # drawer = qml.draw(test)
+    # print(drawer())
+
+
+def run7(inversion):
+    probs = grover7(inversion)
+
+    colormap = np.zeros((8, 16), dtype=float)
+    i = 0
+    j = 0
+    index = 0
+    for x in probs:
+        colormap[i, j] = x
+        index = index + 1
+        i = (i + 1) % 8
+        if i % 8 == 0:
+            j = j + 1
+
+    fig, ax = plt.subplots()
+
+    # ax.yaxis.set_major_formatter(StrMethodFormatter("{x:03b}"))
+    ax.yaxis.set_ticks(range(16))
+    # ax.xaxis.set_major_formatter(StrMethodFormatter("{x:04b}"))
+    ax.xaxis.set_ticks(range(16))
+
+    # plt.ylabel("")
+    plt.xlabel("pixel index")
+    plt.ylabel("grover iteration number")
+
+    plt.imshow(colormap, cmap="Blues", interpolation='nearest')
+    # cax = plt.axes([0.85, 0.1, 0.075, 0.8])
+    # plt.colorbar(cax=cax)
+    plt.show()
+    # drawer = qml.draw(test)
+    # print(drawer())
+
+
+run8(inversion = 6)
+#run7(inversion=6)
+# print(grover7(inversion=5))
---
+++ mygates.py
@@ -0,0 +1,210 @@
+from functools import lru_cache
+
+import pennylane as qml
+from pennylane import numpy as np
+
+class CCCZ(qml.operation.Operation):
+    num_wires = 4
+    num_params = 0
+
+    def adjoint(self):
+        return CCCZ(wires=self.wires)
+
+    def pow(self, z):
+        return super().pow(z % 2)
+
+    @property
+    def control_wires(self):
+        return qml.Wires(self.wires[:3])
+
+    @property
+    def is_hermitian(self):
+        return True
+
+    @staticmethod
+    @lru_cache()
+    def compute_matrix():  # pylint: disable=arguments-differ
+        return np.array(
+            [
+                [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1],
+            ]
+        )
+class CCCCZ(qml.operation.Operation):
+    num_wires = 5
+    num_params = 0
+    """int: Number of trainable parameters that the operator depends on."""
+
+    basis = "Z"
+
+    def label(self, decimals=None, base_label=None, cache=None):
+        return base_label or "Z"
+
+    @staticmethod
+    @lru_cache()
+    def compute_matrix():  # pylint: disable=arguments-differ
+        return np.array(
+            [
+                [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1],
+            ]
+        )
+
+    def adjoint(self):
+        return CCCCZ(wires=self.wires)
+
+    def pow(self, z):
+        return super().pow(z % 2)
+
+    @property
+    def control_wires(self):
+        return qml.Wires(self.wires[:3])
+
+    @property
+    def is_hermitian(self):
+        return True
+
+class Coracle(qml.operation.Operation):
+    num_wires = 5
+    num_params = 1
+    """
+    wires[4] is control wire
+    the gate represents a controllable oracle that inverts inversion number of qubits when control qubit is inverted 
+    """
+
+    def __init__(self, inversion, wires, do_queue=True, id=None):
+
+        super().__init__(inversion, wires=wires, do_queue=do_queue, id=id)
+
+    @staticmethod
+    def compute_decomposition(inversion, wires):
+        match inversion:
+            case 0:
+                return []
+            case 1:
+                return CCCCZ(wires=wires)
+            case 6:
+                return [
+                    qml.CCZ(wires=[wires[0], wires[2], wires[4]]),
+                    qml.CCZ(wires=[wires[1], wires[3], wires[4]]),
+                    qml.RX(np.pi, wires=wires[1]),
+                    qml.RX(np.pi, wires=wires[3]),
+                    CCCZ(wires=[wires[1], wires[2], wires[3], wires[4]]),
+                    qml.RX(np.pi, wires=wires[1]),
+                    qml.RX(np.pi, wires=wires[3]),
+                ]
+
+
+class Cdiffuser(qml.operation.Operation):
+    num_wires = 5
+    num_params = 0
+
+    @staticmethod
+    def compute_decomposition(wires):
+        return [
+            qml.Hadamard(wires=wires[0]),
+            qml.Hadamard(wires=wires[1]),
+            qml.Hadamard(wires=wires[2]),
+            qml.Hadamard(wires=wires[3]),
+            qml.RX(np.pi, wires=wires[0]),
+            qml.RX(np.pi, wires=wires[1]),
+            qml.RX(np.pi, wires=wires[2]),
+            qml.RX(np.pi, wires=wires[3]),
+            CCCCZ(wires=wires),
+            qml.RX(np.pi, wires=wires[0]),
+            qml.RX(np.pi, wires=wires[1]),
+            qml.RX(np.pi, wires=wires[2]),
+            qml.RX(np.pi, wires=wires[3]),
+            qml.Hadamard(wires=wires[0]),
+            qml.Hadamard(wires=wires[1]),
+            qml.Hadamard(wires=wires[2]),
+            qml.Hadamard(wires=wires[3]),
+        ]
+
+
+class Oracle(qml.operation.Operation):
+    num_wires = 4
+    num_params = 0
+
+    @staticmethod
+    def compute_decomposition(wires):
+        return [
+            qml.CZ(wires=[0, 2]),
+            qml.CZ(wires=[1, 3]),
+            qml.RX(np.pi, wires=1),
+            qml.RX(np.pi, wires=3),
+            qml.CCZ(wires=[1, 2, 3]),
+            qml.RX(np.pi, wires=1),
+            qml.RX(np.pi, wires=3),
+        ]
+
+
+class Diffuser(qml.operation.Operation):
+    num_wires = 4
+    num_params = 0
+
+    @staticmethod
+    def compute_decomposition(wires):
+        return [
+            qml.Hadamard(wires=0),
+            qml.Hadamard(wires=1),
+            qml.Hadamard(wires=2),
+            qml.Hadamard(wires=3),
+            qml.RX(np.pi, wires=0),
+            qml.RX(np.pi, wires=1),
+            qml.RX(np.pi, wires=2),
+            qml.RX(np.pi, wires=3),
+            CCCZ(wires=[0, 1, 2, 3]),
+            qml.RX(np.pi, wires=0),
+            qml.RX(np.pi, wires=1),
+            qml.RX(np.pi, wires=2),
+            qml.RX(np.pi, wires=3),
+            qml.Hadamard(wires=0),
+            qml.Hadamard(wires=1),
+            qml.Hadamard(wires=2),
+            qml.Hadamard(wires=3),
+        ]
