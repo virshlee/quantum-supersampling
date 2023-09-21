27173b70e131f906ba8346ea8bb9671216b3b415
virshlee <51300115+virshlee@users.noreply.github.com>
2023-09-21 09:10:06 +0200

    comparators added

---
+++ comparator.py
@@ -0,0 +1,116 @@
+from functools import lru_cache
+
+import pennylane as qml
+from pennylane import numpy as np
+import matplotlib.pyplot as plt
+from mygates import *
+from matplotlib.ticker import StrMethodFormatter
+
+shots = int(np.power(10, 7))
+dev1 = qml.device("default.qubit", wires=[0, 1, 2, 3, 4], shots=shots)
+
+
+class Oracle4(qml.operation.Operation):
+    num_wires = 4
+    num_params = 0
+    """
+    @staticmethod
+    @lru_cache()
+    def compute_matrix():  # pylint: disable=arguments-differ
+        return np.array(
+            [
+                [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1],
+            ]
+        )
+    """
+    @staticmethod
+    def compute_decomposition(wires):
+        return [
+            qml.IntegerComparator(value=12,geq=False,wires=range(5)),
+            qml.CZ(wires=[4, 0]),
+            qml.CZ(wires=[4, 1]),
+            qml.CZ(wires=[4, 2]),
+            qml.CZ(wires=[4, 3]),
+        ]
+
+
+class Diffuser0(qml.operation.Operation):
+    num_wires = 4
+    num_params = 0
+
+    @staticmethod
+    def compute_decomposition(wires):
+        return [
+            qml.Hadamard(wires=0),
+            qml.Hadamard(wires=1),
+            qml.Hadamard(wires=2),
+            qml.Hadamard(wires=3),
+            qml.RX(np.pi, wires=0),
+            qml.RX(np.pi, wires=1),
+            qml.RX(np.pi, wires=2),
+            qml.RX(np.pi, wires=3),
+            CCCZ(wires=[0, 1, 2, 3]),
+            qml.RX(np.pi, wires=0),
+            qml.RX(np.pi, wires=1),
+            qml.RX(np.pi, wires=2),
+            qml.RX(np.pi, wires=3),
+            qml.Hadamard(wires=0),
+            qml.Hadamard(wires=1),
+            qml.Hadamard(wires=2),
+            qml.Hadamard(wires=3),
+        ]
+
+
+@qml.qnode(dev1, interface="autograd")
+def grover4():
+    qml.Hadamard(wires=0)
+    qml.Hadamard(wires=1)
+    qml.Hadamard(wires=2)
+    qml.Hadamard(wires=3)
+    Oracle4(wires=[0, 1, 2, 3])
+    Diffuser0(wires=[0, 1, 2, 3])
+    return qml.probs(wires=range(4))
+
+
+a = grover4()
+print(a)
+c = np.zeros((4, 4), dtype=float)
+i = 0
+j = 0
+index = 0
+for x in a:
+    c[i, j] = x
+    print(index, x)
+    index = index + 1
+    i = (i + 1) % 4
+    if i % 4 == 0:
+        j = j + 1
+
+print(c)
+
+fig, ax = plt.subplots()
+
+# ax.yaxis.set_major_formatter(StrMethodFormatter("{x:02b}"))
+ax.yaxis.set_ticks(range(4))
+# ax.xaxis.set_major_formatter(StrMethodFormatter("{x:02b}"))
+ax.xaxis.set_ticks(range(4))
+
+plt.xlabel("qx")
+plt.ylabel("qy")
+plt.imshow(c, cmap='hot', interpolation='nearest')
+plt.show()
---
+++ comparator2.py
@@ -0,0 +1,73 @@
+from functools import lru_cache
+
+import pennylane as qml
+from pennylane import numpy as np
+import matplotlib.pyplot as plt
+from mygates import *
+from matplotlib.ticker import StrMethodFormatter
+
+shots = int(np.power(10, 2))
+dev1 = qml.device("default.qubit", wires=["a1","b1","target1","a2","b2","target2","a3","b3","target3"], shots=shots)
+
+class Uv(qml.operation.Operation):
+    num_wires = 3
+    num_params = 0
+
+    @staticmethod
+    def compute_decomposition(wires):
+        return [
+            qml.RX(np.pi, wires=wires[1]),
+            qml.Toffoli(wires=wires),
+            qml.CNOT(wires=[wires[0], wires[1]]),
+        ]
+
+class U0(qml.operation.Operation):
+    num_wires = 3
+    num_params = 0
+
+    @staticmethod
+    def compute_decomposition(wires):
+        return [
+            qml.RX(np.pi, wires=wires[1]),
+            qml.Toffoli(wires=wires),
+            qml.RX(np.pi, wires=wires[1]),
+
+        ]
+
+class Comparator3bit(qml.operation.Operation):
+    num_wires = 9
+    num_params = 0
+
+    @staticmethod
+    def compute_decomposition(wires):
+        return [
+            Uv(wires=["a1","b1","target1"]),
+            Uv(wires=["a2", "b2", "target2"]),
+            Uv(wires=["a3", "b3", "target3"]),
+            qml.Toffoli(wires=["b1","target2","target1"]),
+            U0(wires=["a2", "b2", "target2"]),
+            qml.Toffoli(wires=["b1","b2","target2"]),
+            qml.Toffoli(wires=["target2","target3","target1"]),
+            U0(wires=["a3", "b3", "target3"]),
+            qml.Toffoli(wires=["target2","b2","target3"]),
+        ]
+
+@qml.qnode(dev1, interface="autograd")
+def compare_the_bits(a,b):
+    qml.RX(np.pi * a[0], wires="a1"),
+    qml.RX(np.pi * a[1], wires="a2"),
+    qml.RX(np.pi * a[2], wires="a3"),
+    qml.RX(np.pi * b[0], wires="b1"),
+    qml.RX(np.pi * b[1], wires="b2"),
+    qml.RX(np.pi * b[2], wires="b3"),
+    Comparator3bit(wires=["a1","b1","target1","a2", "b2", "target2","a3", "b3", "target3"])
+    return qml.probs(wires=["target1"])
+
+for i1 in range(2):
+    for i2 in range(2):
+        for i3 in range(2):
+            for i4 in range(2):
+                for i5 in range(2):
+                    for i6 in range(2):
+                                a = compare_the_bits(a=[i1,i2,i3],b=[i4,i5,i6])
+                                print("a = ",[i1,i2,i3],"b = ",[i4,i5,i6],"a or b is smaller: ",a)
